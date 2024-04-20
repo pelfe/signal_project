b8fd0c9bb1bdbc75eb86009632fca4e3a8025968
Vincent Viitala <viitala.vincent@gmail.com>
2024-04-20 19:26:26 +0200

    Corrected AlertGenerator.java to follow Google guidelines

--- src/main/java/com/cardio_generator/generators/AlertGenerator.java
+++ src/main/java/com/cardio_generator/generators/AlertGenerator.java
@@ -6,11 +6,16 @@ import com.cardio_generator.outputs.OutputStrategy;
 
 public class AlertGenerator implements PatientDataGenerator {
 
+    
     public static final Random randomGenerator = new Random();
-    private boolean[] AlertStates; // false = resolved, true = pressed
 
+    //Non-constants should be in camelCase changed naming.
+    private boolean[] alertStates; // false = resolved, true = pressed
+
+    //Method names are written on lower camel case. But it's a constructor, so it must
+    //have same name as the class it's from.
     public AlertGenerator(int patientCount) {
-        AlertStates = new boolean[patientCount + 1];
+        alertStates = new boolean[patientCount + 1];
     }
 
     @Override
@@ -23,12 +28,15 @@ public class AlertGenerator implements PatientDataGenerator {
                     outputStrategy.output(patientId, System.currentTimeMillis(), "Alert", "resolved");
                 }
             } else {
-                double Lambda = 0.1; // Average rate (alerts per period), adjust based on desired frequency
-                double p = -Math.expm1(-Lambda); // Probability of at least one alert in the period
+                //All variables should be in lower case camel. Changed naming.
+                double lambda = 0.1; // Average rate (alerts per period), adjust based on desired frequency
+                double p = -Math.expm1(-lambda); // Probability of at least one alert in the period
                 boolean alertTriggered = randomGenerator.nextDouble() < p;
 
                 if (alertTriggered) {
-                    AlertStates[patientId] = true;
+                    //Had to change from AlertStates to alertStates,
+                    // to match the variable name change.
+                    alertStates[patientId] = true;
                     // Output the alert
                     outputStrategy.output(patientId, System.currentTimeMillis(), "Alert", "triggered");
                 }
